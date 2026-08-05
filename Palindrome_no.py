3609468621ae03872361ee6f1e43d87f2883ffc6
komal-devs <wingsofgrace13@gmail.com>
2026-08-05 18:17:10 +0530

    Add files via upload
    Program to check palindrome number

---
+++ Palindrome_no.py
@@ -0,0 +1,11 @@
+# check input number is palindrome or not
+
+n = int(input("Enter a number -- "))
+print("Entered number is -- ",n)
+num = n
+result = 0
+while num> 0:
+  last_digit = num % 10
+  result = (result*10)+ last_digit
+  num = num//10
+print( n == result)
